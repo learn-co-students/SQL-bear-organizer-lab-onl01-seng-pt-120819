20a09f71fdb228ee4b416faeeb7560565c14d397
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-02-18 04:27:23 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive='Y' ORDER BY age ASC"
+  "SELECT name, age FROM bears WHERE alive='1' ORDER BY age ASC"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -19,13 +19,13 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "SELECT color, COUNT(color) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT COUNT(*) FROM bears where temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT name FROM bears WHERE name = NULL"
+  "SELECT * FROM bears WHERE id =8 "
 end
